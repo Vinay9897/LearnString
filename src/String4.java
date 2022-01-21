8cf39c7957b8a06b907ce40f3cd9638efd896d8f
Vinay <vinayyadav91190@gmail.com>
2022-01-21 08:35:17 +0530

    write a program for rotate string by k

--- src/String4.java
+++ src/String4.java
@@ -1,5 +1,24 @@
 public class String4 {
 
-    // wap for t
-    
+    public static void main(String[] args) {
+        String str = "abbas12";
+        System.out.println("MY String : " + str);
+        int rotateby = 2;
+        System.out.println(lefrotate(str, rotateby));
+        System.out.println(rightrotate(str, 3));
+
+    }
+
+    private static String rightrotate(String str, int i) {
+        String ans1 = lefrotate(str, str.length() - i);
+        return ans1;
+    }
+
+    private static String lefrotate(String str, int rotateby) {
+        String ans = str.substring(rotateby) + str.substring(0, rotateby);
+
+        return ans;
+
+    }
+
 }
